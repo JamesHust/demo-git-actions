dcf43c3ce1538ff2cb7e6339a9e3583cf7c8c0b2
hungmt4 <52951651+JamesHust@users.noreply.github.com>
2023-08-02 11:29:57 +0700

    test: update ui homepage and add avatar image

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Avatar" class="logo" src="@/assets/img/avatar.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hi, I'm The Hung" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
